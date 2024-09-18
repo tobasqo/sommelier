a7cdc97cfa1ad5b30fa356dbb5ee0296d6ced585
tobasqo <szymon.j.jaskowski@gmail.com>
2024-09-18 20:13:13 +0200

    browser reload

--- wines/settings.py
+++ wines/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sommelier.apps.SommelierConfig',
+    'django_browser_reload',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'wines.urls'
--- wines/urls.py
+++ wines/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sommelier.urls')),
+    path('__reload__/', include('django_browser_reload.urls')),
 ]
