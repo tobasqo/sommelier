f78edf2f27488af26437f897db14e168ae4f86dd
tobasqo <szymon.j.jaskowski@gmail.com>
2024-09-18 22:15:16 +0200

    style wine form

--- sommelier/forms.py
+++ sommelier/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms import layout
+from crispy_forms.helper import FormHelper
 from django import forms
 from django.forms import DateInput
 
@@ -8,6 +10,30 @@ class WineForm(forms.ModelForm):
     class Meta:
         model = models.Wine
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = layout.Layout(
+            layout.Div(
+                layout.Div('taste', css_class='md:w-[50%]'),
+                layout.Div('kind', css_class='md:w-[50%]'),
+                css_class='md:flex md:justify-between',
+            ),
+            'country',
+            'type',
+            'name',
+            'producer',
+            layout.Div(
+                layout.Submit(
+                    'submit',
+                    'Zapisz',
+                    css_class='rounded-md bg-amber-200 px-4 py-2 text-sm font-semibold shadow-sm hover:bg-amber-500 '
+                              'focus-visible:outline focus-visible:outline-2',
+                ),
+                css_class='flex justify-center mt-4',
+            ),
+        )
 
 
 class BottleForm(forms.ModelForm):
