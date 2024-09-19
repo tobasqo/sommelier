ab6fd3214c583cccb406a231c27bf3a43e873dd5
tobasqo <szymon.j.jaskowski@gmail.com>
2024-09-19 21:59:44 +0200

    style shopinfo form

--- sommelier/forms.py
+++ sommelier/forms.py
@@ -1,7 +1,6 @@
 from crispy_forms import layout
 from crispy_forms.helper import FormHelper
 from django import forms
-from django.forms import DateInput
 
 from sommelier import models
 
@@ -72,18 +71,17 @@ class BottleForm(forms.ModelForm):
                 css_class='flex justify-between',
             ),
             'description',
-            'image',  # want to style this element
+            'image',
             layout.Div(
                 layout.Submit(
                     'submit',
                     'Zapisz',
-                    css_class='rounded-md bg-amber-200 px-4 py-2 text-sm font-semibold shadow-sm hover:bg-amber-300 '
+                    css_class='rounded-md bg-amber-200 px-4 py-2 text-sm font-semibold shadow-sm hover:bg-amber-300'
                               'focus-visible:outline focus-visible:outline-2',
                 ),
                 css_class='flex justify-center mt-4',
             ),
         )
-        self.helper.use_custom_control = True
 
 
 class ShopInfoForm(forms.ModelForm):
@@ -91,5 +89,34 @@ class ShopInfoForm(forms.ModelForm):
         model = models.ShopInfo
         fields = '__all__'
         widgets = {
-            'date': DateInput(attrs={'type': 'date'}),
+            'date': forms.DateInput(attrs={'type': 'date'}),  # TODO: change display format
+            'price': forms.NumberInput(attrs={'value': 20}),
         }
+        labels = {
+            'bottle': 'Butelka',
+            'shop_name': 'Sklep',
+            'price': 'Cena',
+            'date': 'Data zakupu',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = layout.Layout(
+            'bottle',
+            'shop_name',
+            layout.Div(
+                layout.Div('date', css_class='w-[49%]'),
+                layout.Div('price', css_class='w-[49%]'),
+                css_class='flex justify-between',
+            ),
+            layout.Div(
+                layout.Submit(
+                    'submit',
+                    'Zapisz',
+                    css_class='rounded-md bg-amber-200 px-4 py-2 text-sm font-semibold shadow-sm hover:bg-amber-300'
+                              'focus-visible:outline focus-visible:outline-2',
+                ),
+                css_class='flex justify-center mt-1.5',
+            ),
+        )
