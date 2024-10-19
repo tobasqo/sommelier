93b464aa96f2d30fa2e1a4b73fdc750b0a1e7ae5
tobasqo <szymon.j.jaskowski@gmail.com>
2024-10-19 17:51:42 +0200

    todos for admin panel

--- sommelier/admin.py
+++ sommelier/admin.py
@@ -5,11 +5,13 @@ from sommelier import models
 
 @admin.register(models.Wine)
 class WineAdmin(admin.ModelAdmin):
+    # TODO: add inline bottles
     pass
 
 
 @admin.register(models.Bottle)
 class BottleAdmin(admin.ModelAdmin):
+    # TODO: add inline shop infos
     pass
 
 
